9bdba5f8f7bb256e6ad352342f2e57b8d17187a8
Angel Lopez Manriquez <sigma271@outlook.com>
2018-04-21 00:35:00 +0000

    Add files via upload

--- fna c lang/fna.h
+++ fna c lang/fna.h
@@ -26,10 +26,16 @@ static uint _hash(const void *key) {
     return k->id + g_string_hash(k->symbol);
 }
 
-static int _equals(const void *p, const void *q) {
+static int _equals(const void *p, const void *q) { // for compare values
     dpair *a = (dpair *) p;
     dpair *b = (dpair *) q;
     return a->id == b->id && g_string_equal(a->symbol, b->symbol);
+    //vector *a = (vector *) p;
+    //vector *b = (vector *) q;
+    //if (a->size != b->size) return false;
+    //else for (int i = 0; i < a->size; i++)
+    //    if (a->item[i] != b->item[i]) return false;
+    //return true;
 }
 
 fna *new_fna(vector *initial_s, vector *final_s) {
@@ -40,21 +46,21 @@ fna *new_fna(vector *initial_s, vector *final_s) {
     return self;
 }
 
-void fna_add(fna *self, const int id, const char *symbol, int destiny) {
-    dpair *key = malloc(sizeof(dpair)); // build gpair key
-    key->id = id;
-    key->symbol = g_string_new(symbol);
-    vector *bucket;
-    if (g_hash_table_contains(self->delta, key)) {
-        vector *v = (vector *) g_hash_table_lookup(self->delta, key);
-        vpush_back(v, &destiny); // warning
-    } else { // push a vector direction
-        bucket = new_vector();
-        vpush_back(bucket, &destiny);
-        g_hash_table_insert(self->delta, key, bucket);
-        vector *v = (vector *) g_hash_table_lookup(self->delta, key);
-        if (v)  { printf("%d \n", *(int *) vat(bucket, 0)); puts ("pushed"); }
-        else puts("NULL obtained");
+void fna_add(fna *self, dpair *key, int dest) {
+    int *destiny = malloc(sizeof(int)); // ALLOCATE FUCKING MEMORY
+    *destiny = dest;
+    if (g_hash_table_contains((*self).delta, key)) { // get a vector and modify it
+        vector *v = (vector *) g_hash_table_lookup((*self).delta, key); // get
+        vpush_back(v, destiny); 
+        //for (int i = 0; i < v->size; i++)
+        //    printf("%d \n", *(int *) v->item[i]);
+    } else { 
+        vector *bucket = new_vector();
+        vpush_back(bucket, destiny);
+        g_hash_table_insert((*self).delta, key, bucket);
+        vector *w = g_hash_table_lookup((*self).delta, key);
+        //for (int i = 0; i < w->size; i++)
+        //    printf("%d \n", *(int *) w->item[i]);
     }
 }
 
